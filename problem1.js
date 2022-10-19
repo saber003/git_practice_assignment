fcdadc21e8080ed8bcaa12c9a6b183a7fd9fa8df
Nisar0101 <nisarshaikhm18@gmail.com>
2022-10-19 13:02:39 +0530

    change in problem1 done

--- problem1.js
+++ problem1.js
@@ -1,13 +1,13 @@
 // check prime or not
 let N=7;
-let count=2;
+let count=0;
 
 for(let i=1;i<=N;i++){
 	if (N%i==0){
 		count++;
 	}
 }
-if (count=2){
+if (count==2){
 	console.log("Prime Number");
 }else{
 	console.log("Not Prime Number");
